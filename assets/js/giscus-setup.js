2417e2b2be6a0e2a72ebc392a29ad8f4ee9f3749
geunee20 <70561864+geunee20@users.noreply.github.com>
2026-05-05 22:59:45 +0000

    Deploying to gh-pages from @ geunee20/geunee20.github.io@e8ae1b926a69dcd8ad26cf52147f01c2ea88a43b 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -5,11 +5,11 @@ function determineGiscusTheme() {
       document.documentElement.getAttribute("data-theme") ||
       "system";
 
-    if (theme === "dark") return "dark";
-    if (theme === "light") return "light";
+    if (theme === "dark") return "preferred_color_scheme";
+    if (theme === "light") return "preferred_color_scheme";
 
     const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    return prefersDark ? "dark" : "light";
+    return prefersDark ? "preferred_color_scheme" : "preferred_color_scheme";
   
 }
 
@@ -19,16 +19,16 @@ function determineGiscusTheme() {
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
     "data-repo": "geunee20/geunee20.github.io",
-    "data-repo-id": "",
+    "data-repo-id": "R_kgDOSVM5IA",
     "data-category": "Comments",
     "data-category-id": "",
     "data-mapping": "title",
     "data-strict": "1",
     "data-reactions-enabled": "1",
     "data-emit-metadata": "0",
-    "data-input-position": "bottom",
+    "data-input-position": "top",
     "data-theme": giscusTheme,
-    "data-lang": "en",
+    "data-lang": "ko",
     crossorigin: "anonymous",
     async: true,
   };
