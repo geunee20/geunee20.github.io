360ff3fb1316cc44eac79913370db778a795e9e3
geunee20 <70561864+geunee20@users.noreply.github.com>
2026-05-06 20:44:23 +0000

    Deploying to gh-pages from @ geunee20/geunee20.github.io@0bd1f59022aa33150323bfb489dddeb3322450a6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,139 +51,74 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/volunteerings/";
               },
-            },{id: "post-system-modeling-leadscrew",
-        
-          title: "[System Modeling] Leadscrew",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/System-Modeling-01/";
-          
-        },
-      },{id: "post-paper-review-impedance-control-and-performance-measure-of-series-elastic-actuators",
-        
-          title: "[Paper Review] Impedance Control and Performance Measure of Series Elastic Actuators",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/Paper-Review-01/";
-          
-        },
-      },{id: "post-controller-design-day9-model-predictive-control-linearization-amp-euler-discretization",
-        
-          title: "[Controller Design] Day9: Model Predictive Control - Linearization &amp; Euler Discretization",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/Controller-Design-Day-09/";
-          
-        },
-      },{id: "post-controller-design-day8-sliding-mode-control",
-        
-          title: "[Controller Design] Day8: Sliding Mode Control",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/Controller-Design-Day-08/";
-          
-        },
-      },{id: "post-controller-design-day7-state-feedbak-control-lqr-controller-and-energy-shaping",
-        
-          title: "[Controller Design] Day7: State Feedbak Control - LQR Controller and Energy Shaping",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/Controller-Design-Day-07/";
-          
-        },
-      },{id: "post-controller-design-day-6-state-feedback-control-pole-placement-and-bang-bang",
-        
-          title: "[Controller Design] Day 6: State Feedback Control - Pole Placement and Bang-Bang",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/Controller-Design-Day-06/";
-          
-        },
-      },{id: "post-controller-design-day-5-pid-control-with-computed-torque-method",
-        
-          title: "[Controller Design] Day 5: PID Control with Computed Torque Method",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/Controller-Design-Day-05/";
-          
-        },
-      },{id: "post-controller-design-day-4-pid-control-with-gravity-compensation-method",
-        
-          title: "[Controller Design] Day 4: PID Control with Gravity Compensation Method",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/Controller-Design-Day-04/";
-          
-        },
-      },{id: "post-system-modeling-day-3-the-equations-of-motion-and-the-jacobians-of-the-triple-link-mechanism",
-        
-          title: "[System Modeling] Day 3: The Equations of Motion and the Jacobians of the...",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/Controller-Design-Day-03/";
-          
-        },
-      },{id: "post-controller-design-day-2-pid-controller",
-        
-          title: "[Controller Design] Day 2: PID Controller",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/Controller-Design-Day-02/";
-          
-        },
-      },{id: "post-controller-design-day-1-creating-the-multibody-system-using-simscape",
-        
-          title: "[Controller Design] Day 1: Creating the Multibody System Using Simscape",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/Controller-Design-Day-01/";
-          
-        },
-      },{id: "post-controller-design-day-0-kicking-off-the-controller-design-project",
-        
-          title: "[Controller Design] Day 0: Kicking Off the Controller Design Project",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2024/Controller-Design-Day-00/";
-          
-        },
-      },{id: "news-began-pursuing-a-master-s-degree-in-mechanical-engineering-at-georgia-tech",
+            },{id: "dropdown-news",
+              title: "News",
+              description: "",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "/news/";
+              },
+            },{id: "articles-controller-design-day-0-kicking-off-the-controller-design-project",
+          title: '[Controller Design] Day 0: Kicking Off the Controller Design Project',
+          description: "",
+          section: "Articles",handler: () => {
+              window.location.href = "/articles/0001_Controller_Design_0000/";
+            },},{id: "articles-controller-design-day-1-creating-the-multibody-system-using-simscape",
+          title: '[Controller Design] Day 1: Creating the Multibody System Using Simscape',
+          description: "",
+          section: "Articles",handler: () => {
+              window.location.href = "/articles/0002_Controller_Design_0001/";
+            },},{id: "articles-controller-design-day-2-pid-controller",
+          title: '[Controller Design] Day 2: PID Controller',
+          description: "",
+          section: "Articles",handler: () => {
+              window.location.href = "/articles/0003_Controller_Design_0002/";
+            },},{id: "articles-system-modeling-day-3-the-equations-of-motion-and-the-jacobians-of-the-triple-link-mechanism",
+          title: '[System Modeling] Day 3: The Equations of Motion and the Jacobians of the...',
+          description: "",
+          section: "Articles",handler: () => {
+              window.location.href = "/articles/0004_Controller_Design_0003/";
+            },},{id: "articles-controller-design-day-4-pid-control-with-gravity-compensation-method",
+          title: '[Controller Design] Day 4: PID Control with Gravity Compensation Method',
+          description: "",
+          section: "Articles",handler: () => {
+              window.location.href = "/articles/0005_Controller_Design_0004/";
+            },},{id: "articles-controller-design-day-5-pid-control-with-computed-torque-method",
+          title: '[Controller Design] Day 5: PID Control with Computed Torque Method',
+          description: "",
+          section: "Articles",handler: () => {
+              window.location.href = "/articles/0006_Controller_Design_0005/";
+            },},{id: "articles-controller-design-day-6-state-feedback-control-pole-placement-and-bang-bang",
+          title: '[Controller Design] Day 6: State Feedback Control - Pole Placement and Bang-Bang',
+          description: "",
+          section: "Articles",handler: () => {
+              window.location.href = "/articles/0007_Controller_Design_0006/";
+            },},{id: "articles-controller-design-day7-state-feedbak-control-lqr-controller-and-energy-shaping",
+          title: '[Controller Design] Day7: State Feedbak Control - LQR Controller and Energy Shaping',
+          description: "",
+          section: "Articles",handler: () => {
+              window.location.href = "/articles/0008_Controller_Design_0007/";
+            },},{id: "articles-controller-design-day8-sliding-mode-control",
+          title: '[Controller Design] Day8: Sliding Mode Control',
+          description: "",
+          section: "Articles",handler: () => {
+              window.location.href = "/articles/0009_Controller_Design_0008/";
+            },},{id: "articles-controller-design-day9-model-predictive-control-linearization-amp-euler-discretization",
+          title: '[Controller Design] Day9: Model Predictive Control - Linearization &amp;amp; Euler Discretization',
+          description: "",
+          section: "Articles",handler: () => {
+              window.location.href = "/articles/0010_Controller_Design_0009/";
+            },},{id: "articles-paper-review-impedance-control-and-performance-measure-of-series-elastic-actuators",
+          title: '[Paper Review] Impedance Control and Performance Measure of Series Elastic Actuators',
+          description: "",
+          section: "Articles",handler: () => {
+              window.location.href = "/articles/0011_Paper_Review_0001/";
+            },},{id: "articles-system-modeling-leadscrew",
+          title: '[System Modeling] Leadscrew',
+          description: "",
+          section: "Articles",handler: () => {
+              window.location.href = "/articles/0012_System_Modeling_0001/";
+            },},{id: "news-began-pursuing-a-master-s-degree-in-mechanical-engineering-at-georgia-tech",
           title: 'Began pursuing a Master’s degree in Mechanical Engineering at Georgia Tech!',
           description: "",
           section: "News",},{id: "news-volunteered-at-the-atlanta-science-festival",
@@ -208,52 +143,87 @@ ninja.data = [{
           title: 'eBooth',
           description: "School Event Community Application: Connecting and Boosting Student Events",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project_ebooth/";
+              window.location.href = "/projects/0004_ebooth/";
             },},{id: "projects-make-cornhole-playable-again",
           title: 'Make Cornhole Playable Again',
           description: "Adaptive Cornhole Assistant Tools for People with Spinal Cord Injuries",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project_cornhole/";
+              window.location.href = "/projects/0001_cornhole/";
             },},{id: "projects-setgo",
           title: 'SetGo',
           description: "Route Generating Application: Randomly Generates Jogging Routes Based on Distance",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project_setGo/";
+              window.location.href = "/projects/0005_setgo/";
             },},{id: "projects-triple-pendulum",
           title: 'Triple Pendulum',
           description: "A Computational Tool to Mathematically Estimate the Motion of a Triple Pendulum.",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project_triple_pendulum/";
+              window.location.href = "/projects/0003_triple_pendulum/";
             },},{id: "projects-robot-arm-optimization",
           title: 'Robot Arm Optimization',
           description: "Finite Element Analysis for Durability and Weight Optimization in Robot Arm Design",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project_robot_arm/";
+              window.location.href = "/projects/0002_robot_arm/";
             },},{id: "projects-controller-design",
           title: 'Controller Design',
           description: "Exploring and implementing a spectrum of control strategies for inverted pendulum systems, from classical PID to advanced adaptive techniques",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/6_Controller_Design/";
+              window.location.href = "/projects/0006_controller_design/";
             },},{id: "teachings-fall-23-39-midterm-1-review-session",
           title: 'Fall 23&amp;#39; Midterm 1 Review Session',
           description: "",
           section: "Teachings",handler: () => {
-              window.location.href = "/teachings/01_23_Fa_MT1/";
+              window.location.href = "/teachings/0001_gatech_me4452/0001_23_Fa_MT1/";
             },},{id: "teachings-fall-23-39-midterm-2-review-session",
           title: 'Fall 23&amp;#39; Midterm 2 Review Session',
           description: "",
           section: "Teachings",handler: () => {
-              window.location.href = "/teachings/02_23_Fa_MT2/";
+              window.location.href = "/teachings/0001_gatech_me4452/0002_23_Fa_MT2/";
             },},{id: "teachings-spring-24-39-midterm-1-review-session",
           title: 'Spring 24&amp;#39; Midterm 1 Review Session',
           description: "",
           section: "Teachings",handler: () => {
-              window.location.href = "/teachings/03_24_Sp_MT1/";
+              window.location.href = "/teachings/0001_gatech_me4452/0003_24_Sp_MT1/";
             },},{id: "teachings-spring-24-39-midterm-2-review-session",
           title: 'Spring 24&amp;#39; Midterm 2 Review Session',
           description: "",
           section: "Teachings",handler: () => {
-              window.location.href = "/teachings/04_24_Sp_MT2/";
+              window.location.href = "/teachings/0001_gatech_me4452/0004_24_Sp_MT2/";
+            },},{id: "teachings-index",
+          title: 'Index',
+          description: "",
+          section: "Teachings",handler: () => {
+              window.location.href = "/teachings/0001_gatech_me4452/index/";
+            },},{id: "teachings-index",
+          title: 'Index',
+          description: "",
+          section: "Teachings",handler: () => {
+              window.location.href = "/teachings/0002_utaustin_me350R/index/";
+            },},{id: "volunteerings-index",
+          title: 'Index',
+          description: "",
+          section: "Volunteerings",handler: () => {
+              window.location.href = "/volunteerings/0001_rural_education/index/";
+            },},{id: "volunteerings-index",
+          title: 'Index',
+          description: "",
+          section: "Volunteerings",handler: () => {
+              window.location.href = "/volunteerings/0002_sg_maple/index/";
+            },},{id: "volunteerings-index",
+          title: 'Index',
+          description: "",
+          section: "Volunteerings",handler: () => {
+              window.location.href = "/volunteerings/0003_atlanta_science_festival/index/";
+            },},{id: "volunteerings-index",
+          title: 'Index',
+          description: "",
+          section: "Volunteerings",handler: () => {
+              window.location.href = "/volunteerings/0004_stem_atl/index/";
+            },},{id: "volunteerings-index",
+          title: 'Index',
+          description: "",
+          section: "Volunteerings",handler: () => {
+              window.location.href = "/volunteerings/0005_summer_science/index/";
             },},{
         id: 'social-email',
         title: 'email',
